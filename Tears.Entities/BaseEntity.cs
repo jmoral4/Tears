3f810adb4b18d1844bc0caa22cb60d95936b628d
Jonathan <jmoral4@gmail.com>
2014-07-10 09:45:06 -0400

    Basic entity system and plan doc
    basic entities divided into conceptual, dynamic and static

---
+++ Tears.Entities/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tears.Entities
+{
+    abstract class BaseEntity
+    {
+        public Guid ObjectId { get; set; }
+        public string Name { get; set; }
+
+    }
+}
