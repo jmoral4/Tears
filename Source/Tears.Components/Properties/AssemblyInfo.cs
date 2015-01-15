32ac03aa6e44fc5c8b87d467fce63bd8c319be81
Jonathan <jmoral4@gmail.com>
2015-01-14 19:05:22 -0500

    Added all MonoGame libs + finished update
    Finished updating projects

--- Source/Tears.Components/Properties/AssemblyInfo.cs
+++ Source/Tears.Components/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Tears.Components")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4ccbca87-4842-44fa-ad05-11540d8831d2")]
+[assembly: Guid("0f5f7137-0def-4db3-b3b5-9c4ed76477e5")]
 
 // Version information for an assembly consists of the following four values:
 //
